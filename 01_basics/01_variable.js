889095168e2be4ad239c5fe2690d0eec09b8c878
7abhishe <83947791+7abhishe@users.noreply.github.com>
2023-10-16 05:39:28 +0000

    variable defination

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const accID=12345;
+let accountMail="abhishek@google.com";
+var password="12345";
+ let accountcity="hajipur";
+
+ accountMail="abhi@ctc.edu";
+ let accountstate;
+ `/*
+ always prefer to use let not variable mostly.
+ beacsue it chnges scope of all the elements.
+ */`
+
+
+ console.log(accountMail);
+
+ console.table([accID,accountMail,password,accountcity,accountMail,accountstate]);

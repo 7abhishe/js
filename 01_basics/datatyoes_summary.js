b841fc8eb8abc8217c9b24bc95d250dbec6c0ecc
7abhishe <83947791+7abhishe@users.noreply.github.com>
2023-10-16 18:28:20 +0000

    datatype_summary

---
+++ 01_basics/datatyoes_summary.js
@@ -0,0 +1,25 @@
+//primitive data tyoes
+
+// basically datatypes create only on 2 basis and the the 2 basis are directly assigns values.
+
+//7 types of data types:
+
+//Number,string,booolean,null,undefined,symbols,BigInt
+
+//non-primitve data ypes are  refrence data types:
+
+//array,objects, functions;
+
+const heros=["shaktiman","nagraj","ganeshlal","krish"]
+let myobj{
+            age: 11;
+            name: "anand";
+}
+
+const myfunction =function()
+{
+    console.log("hello world");
+}
+
+console.log(typeof myobj);
+

d435dd4d3c5197a76558416e323424584b588b40
7abhishe <83947791+7abhishe@users.noreply.github.com>
2023-10-16 13:08:09 +0000

    conversion of dattypes

---
+++ 01_basics/03_dataconver.js
@@ -0,0 +1,28 @@
+let score=33;
+
+
+//const {score}=req.body;
+
+console.log(typeof score)
+console.log(typeof score)
+
+
+let theme="33";
+
+
+
+console.log(typeof theme)
+console.log(typeof theme)
+
+
+let valueinNumber=Number(theme);
+console.log(typeof valueinNumber)
+
+let abhi ="abhishek";
+let conver=Number(abhi);
+console.log(conver);
+
+//those datatypes not able to convert into other data types we called them NAN
+
+//true=1
+///false=0;

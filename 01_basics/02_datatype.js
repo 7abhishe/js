921a41f1c2bacfb32413323e4b8233f3df5f305e
7abhishe <83947791+7abhishe@users.noreply.github.com>
2023-10-16 12:45:33 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,19 @@
+"use strict"; // this is for the  modern version of the java script
+
+// alert(3+3) this is the need of web browser. not for the node js.
+ //code readablity should be better for the examples;
+ console.log("abhishek"); 
+ console.log("bittu");
+
+ // number belongs to the 2 to 2^ 53
+ // but if the database desired to be greater than that  then we have to shift on the BIGINT;
+ //boolean for the false and true only
+ // string should be used with parenthesis or not;
+ //undefined means if defined but no use of it on the system
+
+ //NULL standalone ( khali nahi woh empty hai bass);
+ //symbol=> just like a primary ley of database;
+
+ // object
+
+ console.log( typeof null);

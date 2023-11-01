e36694fd068ec2932e6bfbfb1da91662c1c7ca64
7abhishe <83947791+7abhishe@users.noreply.github.com>
2023-11-01 11:21:16 +0000

    stack and heap

--- 01_basics/first.js
+++ 01_basics/first.js
@@ -1,1 +1,25 @@
 console.log("abhishek");
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+/* stack  and heap in the javascript */
+let myyoutubename="abhishekdotcom"
+
+let anothername=myyoutubename;
+
+console.log(myyoutubename)
+console.log(anothername)
+
+let userOne=
+{
+    email:"abhishek@123"
+    upiid:12
+}
+let usertwo={
+    email:"abhi@two"
+    upi:13
+}
+
+// here the heap answer which were being provided in the way of the 
+//as non primitive memory allocation using the heap memeory
+
+//stack uses primative memor storage

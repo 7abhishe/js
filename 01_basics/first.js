3809e02625bd642e79447e7298445380afe4ab1b
7abhishe <83947791+7abhishe@users.noreply.github.com>
2023-11-02 05:11:12 +0000

    ls

--- 01_basics/first.js
+++ 01_basics/first.js
@@ -6,17 +6,19 @@ let myyoutubename="abhishekdotcom"
 
 let anothername=myyoutubename;
 
+anothername="silent_drag"
+
 console.log(myyoutubename)
 console.log(anothername)
 
 let userOne=
 {
-    email:"abhishek@123"
-    upiid:12
+    email:"abhishek@123",
+    upi:"123@sbi"
 }
 let usertwo={
-    email:"abhi@two"
-    upi:13
+    email:"abhi@two",
+    upi:"213@upi"
 }
 
 // here the heap answer which were being provided in the way of the 
---
+++ 01_basics/string_math_number.js
@@ -0,0 +1,13 @@
+// let text = "apple,orange, pineapple";
+// let part = text.slice(2, 10);
+// console.log(part);
+// let aand= text.slice(-1, -10);
+// console.log(aand);
+
+
+console.log(Math);
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
